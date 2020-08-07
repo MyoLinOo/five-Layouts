21680ccb1e298558acba2ee367a6b8f0ef50d92b
myolinoo <myolinoo088555@gmail.com>
2020-08-07 16:40:05 +0630

    first

---
+++ app/src/main/java/com/myogardener/myo_project1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.myogardener.myo_project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.our_team_page)
+
+    }
+}
